203d65e0f7ff0aae99a376edbb548b5a5a9faba7
mahdiar <mahdirmazraei@gmail.com>
2024-03-06 17:04:50 +0330

    create

---
+++ main.py
@@ -0,0 +1,111 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from database import SessionLocal, engine
+import models
+from pydantic import BaseModel
+from fastapi import Body
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi import FastAPI, HTTPException, Response, Cookie
+from jose import jwt, ExpiredSignatureError
+
+SECRET_KEY = "3faa53f02b282c59494d692e1dbb8f1c7f7a820607a9e6a783c40592b6dd0ce0"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+app = FastAPI()
+
+class UserSignin(BaseModel):
+    username: str
+    password: str
+    # example_cookie: str = Cookie(None, description="Example cookie for authentication purposes.")
+
+class UserSignup(UserSignin):
+    email: str
+# ایجاد جداول در دیتابیس (اگر وجود نداشته باشد)
+models.Base.metadata.create_all(bind=engine)
+
+# Dependency برای دریافت اتصال به دیتابیس
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# تابع برای ایجاد توکن
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# کلاس برای ایجاد یک instance از CryptContext
+class PasswordHasher:
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def get_password_hash(self, password):
+        return self.pwd_context.hash(password)
+
+pwd_context = PasswordHasher()
+
+# API برای ثبت‌نام
+@app.post("/register/")
+def register(response: Response ,user: UserSignup = Body(...),token_cookie: str = Cookie(None),db: Session = Depends(get_db)):
+    if token_cookie != None:
+        try:
+            payload = jwt.decode(token_cookie, SECRET_KEY, algorithms=[ALGORITHM])
+            exp_timestamp = payload.get("exp")
+            expiration_datetime = datetime.utcnow() + timedelta(seconds=exp_timestamp)
+            if expiration_datetime > datetime.utcnow():
+                return {"message": "you eldearly logn"}
+        except:
+
+            token_cookie = create_access_token({"username" : user.username})
+            response.set_cookie(key="token_cookie", value = token_cookie)
+            hashed_password = pwd_context.get_password_hash(user.password)
+            db_user = models.User(username=user.username, email=user.email, hashed_password=hashed_password,)
+            db.add(db_user)
+            db.commit()
+            db.refresh(db_user)
+            return db_user
+
+# API برای ورود
+@app.post("/login/")
+def login(response: Response ,users: UserSignin = Body(...),token_cookie: str = Cookie(None), db: Session = Depends(get_db)):
+    if token_cookie:
+        try:
+            payload = jwt.decode(token_cookie, SECRET_KEY, algorithms=["HS256"])
+            exp_timestamp = payload.get("exp")
+            expiration_datetime = datetime.utcnow() + timedelta(seconds=exp_timestamp)
+            if expiration_datetime > datetime.utcnow():
+                return {"message": "you eldearly login"}
+        except ExpiredSignatureError:
+
+        # payload = jwt.decode(token_cookie, SECRET_KEY, algorithms=[ALGORITHM])
+        # exp_timestamp = payload.get("exp")
+        # expiration_datetime = datetime.utcnow() + timedelta(seconds=exp_timestamp)
+        # if expiration_datetime > datetime.utcnow():
+        #     return {"message": "you eldearly login"}
+            user = db.query(models.User).filter(models.User.username == users.username).first()
+            if not user or not pwd_context.verify_password(users.password, user.hashed_password):
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid username or password",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            token_cookie = create_access_token({"username" : users.username})
+            response.set_cookie(key="token_cookie", value = token_cookie)
+            return {"message": "Login successful"}
